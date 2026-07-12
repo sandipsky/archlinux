3f9f2be1818ca2f6b9d29998646a2c4b56430433
sandipsky <sandipshakya75@gmail.com>
2026-07-12 14:44:37 +0545

    batteery

--- install.sh
+++ install.sh
@@ -224,7 +224,7 @@ StartLimitBurst=0
 
 [Service]
 Type=oneshot
-ExecStart=/usr/bin/find /sys/class/power_supply -name charge_control_end_threshold -exec bash -c 'echo 80 > "{}"' \;
+ExecStart=/usr/bin/find -L /sys/class/power_supply -maxdepth 2 -name charge_control_end_threshold -exec bash -c 'echo 80 > "$1"' _ {} \;
 
 [Install]
 WantedBy=multi-user.target suspend.target hibernate.target hybrid-sleep.target suspend-then-hibernate.target
